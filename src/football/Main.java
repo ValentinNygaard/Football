b00e6ae48f1c87b8a363e62d054b03f9c36fbb7a
ValentinNygaard <rene930d@stud.kea.dk>
2018-09-12 21:53:45 +0200

    hej

--- src/football/Main.java
+++ src/football/Main.java
@@ -3,6 +3,6 @@ package football;
 public class Main {
 
     public static void main(String[] args) {
-	// write sasfasdfasdfdfasdfasdASDyour code here
+	// write sasfasdfasdfdfasdfasSFASDFdASDyour code here
     }
 }
